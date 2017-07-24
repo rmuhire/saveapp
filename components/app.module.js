5e3d85a2fc803a3282765bedb270f9311c698ba1
luc <luctunechi45@gmail.com>
2017-07-24 13:24:25 +0200

    Save-app add

---
+++ components/app.module.js
@@ -0,0 +1,1 @@
+var save = angular.module('save', ['ngRoute']);
---
+++ components/app.route.js
@@ -0,0 +1,36 @@
+save.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl:'components/home/homeView.html'
+    })
+    .when('/signin',{
+        templateUrl:'components/signin/loginView.html'
+    })
+    .when('/signup',{
+        templateUrl:'components/signup/signupView.html'
+    })
+    .when('/forget-password',{
+        templateUrl:'components/forget-password/forgetView.html'
+    })
+    .when('/recover-password',{
+        templateUrl:'components/changing-password/changingView.html'
+    })
+    .when('/project',{
+        templateUrl:'components/project/projectView.html'
+    })
+    .when('/saving-group',{
+        templateUrl:'components/saving-group/sgView.html'
+    })
+    .when('/settings',{
+        templateUrl:'components/setting/settingView.html'
+    })
+    .when('/map',{
+        templateUrl:'components/map/mapView.html'
+    })
+    .when('/404',{
+        templateUrl:'components/404/404.html'
+    })
+    .otherwise({
+        redirectTo: '/404'    
+    });
+}]);
