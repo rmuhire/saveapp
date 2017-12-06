9ca7941049c5373bbf440cd860eb2466eec1454c
remymuhire <jeanremy1806@gmail.com>
2017-12-06 16:47:27 +0200

    app module

--- components/app.module.js
+++ components/app.module.js
@@ -3,4 +3,4 @@ var save = angular.module('save', ['ngRoute','base64','ngCookies'])
                       var auth = $base64.encode("eyJhbGciOiJIUzI1NiIsImV4cCI6MTUwODQwMzgxNiwiaWF0IjoxNTA4MzE3NDE2fQ.eyJpZCI6MX0.4uZ7CfrQz_AXz5ErwnwzNTsh67phhNx-qBATMcXE_X0:");
                       $httpProvider.defaults.headers.common['Authorization'] = 'Basic ' + auth;
                       $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-                  }); 
+                  });  
