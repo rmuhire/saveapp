c41621e16fce811fed4f1b9723756cc4468a3560
remymuhire <jeanremy1806@gmail.com>
2017-12-06 16:49:34 +0200

    name displayed

--- shared/navbar/navbarController.js
+++ shared/navbar/navbarController.js
@@ -1,4 +1,5 @@
-save.controller('navbarCtrl', function($scope, $location, AuthService){
+save.controller('navbarCtrl', function($scope, $location, $rootScope, AuthService){
+    // Sign out controller
     $scope.sign_out = function(){
         AuthService.sign_out()
                    .then(function(){
@@ -8,4 +9,23 @@ save.controller('navbarCtrl', function($scope, $location, AuthService){
                        console.log("fail signed out");
                    })
     }
+    
+    
+    
+    // User profile 
+    $scope.users_info = function(){
+        var user_profile = AuthService.getUserInformations();
+        user_profile.then(function(data){
+            console.log(data);
+            $scope.user_names = data.name != null ? data.name : data.username;
+            $scope.organization = data.organization.name.toUpperCase();
+            console.log($scope.user_names);
+        });
+    }
+    
+    $scope.users_info();
+   
+    
+    
+    
 })
