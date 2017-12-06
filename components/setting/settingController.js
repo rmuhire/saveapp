3b881a5254b463bedcb28aab3722cd15297c602c
remymuhire <jeanremy1806@gmail.com>
2017-12-06 16:48:26 +0200

    settingsController

--- components/setting/settingController.js
+++ components/setting/settingController.js
@@ -1,12 +1,9 @@
-save.controller('settingViewCtrl', ['$scope', '$http','$location', function($scope,$http,$location){
+save.controller('settingViewCtrl', function($scope,$http,$location, $rootScope, AuthService){
   $(function () {
 
     $(".setting").css("height", function(index){
         return $(window).height();
     })
-  //   $(".well").css("height", function(index){
-  //     return $(window).height();
-  // })
     $('#ddlCars01').multiselect({
     includeSelectAllOption: true
 
@@ -15,4 +12,19 @@ save.controller('settingViewCtrl', ['$scope', '$http','$location', function($sco
     includeSelectAllOption: true
    });
  });
-}])
+    
+    
+    $scope.users_info = function(){
+        var user_profile = AuthService.getUserInformations();
+        user_profile.then(function(data){
+            console.log(data);
+            $scope.user_names = data.name != null ? data.name : data.username;
+            $scope.organization = data.organization.name.toUpperCase();
+            console.log($scope.user_names);
+        });
+    }
+    
+    $scope.users_info();
+    
+    
+})
