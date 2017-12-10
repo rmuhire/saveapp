f0477eb486e41d5b46c25ed49f15fdf5e14a029e
remymuhire <jeanremy1806@gmail.com>
2017-12-10 13:03:47 +0200

    project controllers

--- components/project/projectController.js
+++ components/project/projectController.js
@@ -1,8 +1,21 @@
-save.controller('projectCtrl', ['$scope', '$http','$location', function($scope,$http,$location){
+save.controller('projectCtrl', function($scope,$http,$location, ProjectService){
   $(".panel").css("height", function(index){
                     return $(window).height();
                 })
   $(".well").css("height", function(index){
                     return $(window).height();
                 })
-}])
+    
+    
+    // Project Controller
+    
+    $scope.getOrganizationProject = function(){
+        var organization_project = ProjectService.getOrganizationProject();
+        organization_project.then(function(response){
+            console.log(response.data.projects);
+            $scope.projects = response.data.projects;
+        });
+    }
+    $scope.getOrganizationProject();
+    
+})
