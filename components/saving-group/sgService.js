57a7fc080d091be424c3ea7f18097c7a5a8349f7
remymuhire <jeanremy1806@gmail.com>
2017-12-14 23:22:03 +0200

    service js

---
+++ components/saving-group/sgService.js
@@ -0,0 +1,13 @@
+ save.factory('SavingGroupService', function($cookieStore, $q, $timeout, $http){
+     return ({
+         getOrganizationSG: function(){
+             var url = 'http://127.0.0.1:5000/v1/organizations/'+$cookieStore.get('__save_o')+'/sg/';
+             return $http.get(url).then(function(response){
+                 //console.log(response);
+                 return response;
+             }).catch(function(response){
+                 return response;
+             })
+         }
+     })
+ })
