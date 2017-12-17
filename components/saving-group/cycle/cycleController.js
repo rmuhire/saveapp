687dd3051f1898458fd776e5cd3efe4f57713c07
remymuhire <jeanremy1806@gmail.com>
2017-12-18 00:01:24 +0200

    cycle controller

--- components/saving-group/cycle/cycleController.js
+++ components/saving-group/cycle/cycleController.js
@@ -1,88 +1,136 @@
-save.controller('cycleViewCtrl', function($scope, $http, $location){
-    
-    
-    $scope.$on('LoadSgCycle', function(event, opt){
-        var data = [{
-          values: [16, 15, 12],
-          labels: ['US', 'China', 'European Union'],
-          domain: {
-            x: [0, .48]
-          },
-          name: 'GHG Emissions',
-          hoverinfo: 'label+percent',
-          hole: .4,
-          type: 'pie'
-        }];
+save.controller('cycleViewCtrl', function($scope, $http, $location, SGCycleService) {
 
-        var layout = {
-          annotations: [
-            {
-              font: {
-                size: 20
-              },
-              showarrow: false,
-              text: '',
-              x: 0.17,
-              y: 0.5
+
+    $scope.$on('LoadSgCycle', function(event, opt) {
+        // get Cycles
+        console.log(opt.sg)
+        $scope.status = opt.sg.status ? "Graduated" : "Supervised";
+        SGCycleService.getSgCycles(opt.sg.cycle_url).then(function(response) {
+            console.log(response.data.cycles)
+            if (response.data.cycles.length !== 0) {
+                $scope.cycle_length = response.data.cycles.length
+                $scope.first_cycle = response.data.cycles[0].start
+
+                // get cycles members
+                var cycles = response.data.cycles
+                var values = new Array()
+                var labels = new Array()
+                var suggestions = new Array('First', 'Second', 'Third', 'Fourth', 'Fifth')
+                cycles.forEach(function(element, index) {
+                    console.log(element.members_url)
+                    SGCycleService.getCyclesMembers(element.members_url)
+                        .then(function(response) {
+                            console.log(response.data.members.length)
+                            values.push(response.data.members.length)
+                            labels.push(suggestions[index])
+                            $scope.pieChart(values, labels)
+                            
+                        })
+                        .catch(function(response) {
+                            console.log(response)
+                        })
+                })
+
+                
+
             }
-            
-          ],
-          width: 580,
-            height: 400,
-          legend:{
-            orientation	: 'v',
-            x:0,
-            y:-0.2
-          },
-            margin: {
+
+        }).catch(function(response) {
+            console.log(response)
+        })
+
+
+
+        $scope.pieChart = function(values, labels) {
+            console.log(values, labels)
+            var data = [{
+                values: values,
+                labels: labels,
+                domain: {
+                    x: [0, .48]
+                },
+                name: 'Members per cycle',
+                hoverinfo: 'label+percent',
+                hole: .4,
+                type: 'pie'
+            }];
+
+            var layout = {
+                annotations: [{
+                        font: {
+                            size: 20
+                        },
+                        showarrow: false,
+                        text: '',
+                        x: 0.17,
+                        y: 0.5
+                    }
+
+                ],
+                width: 580,
+                height: 400,
+                legend: {
+                    orientation: 'v',
+                    x: 0,
+                    y: -0.2
+                },
+                margin: {
                     l: 0,
                     r: 0,
                     t: 0,
                     b: 0
                 }
+            };
+
+            Plotly.newPlot('sg_cycle', data, layout, {
+                displayModeBar: false
+            });
+        }
+
+
+
+        var trace1 = {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [20, 14, 23],
+            name: 'SF Zoo',
+            type: 'bar'
         };
 
-        Plotly.newPlot('sg_cycle', data, layout, {displayModeBar: false});
-        
-        
-        var trace1 = {
-              x: ['giraffes', 'orangutans', 'monkeys'],
-              y: [20, 14, 23],
-              name: 'SF Zoo',
-              type: 'bar'
-            };
+        var trace2 = {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [12, 18, 29],
+            name: 'LA Zoo',
+            type: 'bar'
+        };
 
-            var trace2 = {
-              x: ['giraffes', 'orangutans', 'monkeys'],
-              y: [12, 18, 29],
-              name: 'LA Zoo',
-              type: 'bar'
-            };
+        var data = [trace1];
 
-            var data = [trace1, trace2];
-
-            var layout = {
-                barmode: 'group',
-                width: 480,
+        var layout = {
+            barmode: 'group',
+            width: 480,
             height: 400,
-          legend:{
-            orientation	: 'h',
-            x:0,
-            y:-0.2
-          },
+            legend: {
+                orientation: 'h',
+                x: 0,
+                y: -0.2
+            },
             margin: {
-                    l: 20,
-                    r: 0,
-                    t: 0,
-                    b: 0
-                }
-                };
+                l: 20,
+                r: 0,
+                t: 0,
+                b: 40
+            }
+        };
 
-            Plotly.newPlot('sg_cycle_money', data, layout, {displayModeBar: false});
-
-
+        Plotly.newPlot('sg_cycle_money', data, layout, {
+            displayModeBar: false
         });
-    
-    
-    
+
+
+
+
+    });
+
+
+
 })
