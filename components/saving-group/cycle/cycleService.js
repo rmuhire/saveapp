cab544aa9a71865409e60db1c284d0d94e6ab838
remymuhire <jeanremy1806@gmail.com>
2017-12-18 00:01:39 +0200

    cycle service

---
+++ components/saving-group/cycle/cycleService.js
@@ -0,0 +1,18 @@
+save.factory('SGCycleService', function($cookieStore, $q, $timeout, $http){
+  return ({
+      getSgCycles: function(url){
+          return $http.get(url).then(function(response){
+              return response
+          }).catch(function(response){
+              return response
+          })
+      },
+      getCyclesMembers: function(url){
+          return $http.get(url).then(function(response){
+              return response
+          }).catch(function(response){
+              return response
+          })
+      }
+  })  
+}) 
