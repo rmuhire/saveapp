887cac29e909584abe8302ca2f9d03cf638d5662
remymuhire <jeanremy1806@gmail.com>
2017-12-18 00:00:50 +0200

    sg service

--- components/saving-group/sgService.js
+++ components/saving-group/sgService.js
@@ -8,6 +8,14 @@
              }).catch(function(response){
                  return response;
              })
+         },
+         getProjectSG: function(id){
+             var url = 'http://127.0.0.1:5000/v1/projects/'+id+'/sg/';
+             return $http.get(url).then(function(response){
+                return response;
+             }).catch(function(response){
+                return response;  
+             });
          }
      })
- }) 
+ })  
