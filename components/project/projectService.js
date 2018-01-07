0144a10fbf4bd4c90ea394a56afabf34693f06ac
remymuhire <jeanremy1806@gmail.com>
2018-01-07 16:48:26 +0200

    project service

--- components/project/projectService.js
+++ components/project/projectService.js
@@ -2,7 +2,7 @@ save.factory('ProjectService', function($cookieStore, $q, $timeout, $http){
    return ({
        getOrganizationProject: function(data){
            var deffered = $q.defer();
-           var url = 'http://localhost:5000/v1/organizations/'+ $cookieStore.get('__save_o')+'/projects/';
+           var url = 'http://197.243.18.25:5000/v1/organizations/'+ $cookieStore.get('__save_o')+'/projects/';
            return $http.get(url)
                .then(function(response){
                     return response;
