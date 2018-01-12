1609a3018f1cd3a85f14850f15389264033c6f90
remymuhire <jeanremy1806@gmail.com>
2018-01-12 14:26:38 +0200

    key controller

---
+++ components/forget-password/keyController.js
@@ -0,0 +1,18 @@
+save.controller('keyController', function($scope, $location, $routeParams, AuthService, $window){
+    console.log($routeParams.keyu, $routeParams.email);
+    var url='http://127.0.0.1:5000/v1/users/'+$routeParams.keyu+'/reset/';
+    AuthService.Key(url)
+        .then(function(response){
+            console.log(response)
+            if (response.status == 200){
+                $window.location.href = "#/change-password/" +$routeParams.email;
+            }else{
+                $window.location.href = "#/signin";
+            }
+            
+        })
+        .catch(function(response){
+            console.log(response)
+            $window.location.href = "#/signin";
+        })
+})
