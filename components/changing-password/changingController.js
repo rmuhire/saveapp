d065c3a5eb6577ff8b9aa653b99d103b009b9df1
remymuhire <jeanremy1806@gmail.com>
2018-01-24 09:26:40 +0200

    changing controller

--- components/changing-password/changingController.js
+++ components/changing-password/changingController.js
@@ -1,11 +1,33 @@
-save.controller('ChangePasswordCtrl', function($scope, $http, $location, $routeParams, AuthService){
+save.controller('ChangePasswordCtrl', function($scope, $http, $location, $routeParams, SettingService){
     
+    $scope.confirmation = false;
+    $scope.change_password = true;
     $scope.changePassword = function(){
         console.log($scope.user, $routeParams.email);
         
-        if ($scope.user['password'] == $scope.user['c_password']){
-            AuthService.
+        if ($scope.user['new_password'] == $scope.user['c_password']){
+            var url = "http://127.0.0.1:5000/v1/users/"+$routeParams.email+"/email/";
+            SettingService.UserInformationEmail(url).then(function(response){
+                $scope.modifyPassword(response.data.id, $scope.user);
+            }).catch(function(response){
+                console.log(response);
+            })
         }
     }
     
-}); 
+    
+    $scope.modifyPassword = function(user_id, user){
+        var url = "http://127.0.0.1:5000/v1/users/"+user_id+"/reset-password/";
+        SettingService.userResetPassword(url, user)
+            .then(function(){
+                console.log("Reset Successfull");
+                $scope.confirmation = true;
+                $scope.change_password = false;
+            })
+            .catch(function(){
+                console.log("Fail Reset");
+            })
+        
+    }
+    
+});  
