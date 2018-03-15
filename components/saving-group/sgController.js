2f639d1f6e262108d5a8b7c4b693f48c202ef950
remymuhire <jeanremy1806@gmail.com>
2018-03-15 18:12:04 +0200

    controller

--- components/saving-group/sgController.js
+++ components/saving-group/sgController.js
@@ -10,8 +10,7 @@ save.controller('sgViewCtrl', function($scope,$http,$location, ProjectService, S
 
     $('#sg_project').multiselect({
         includeSelectAllOption: true,
-        buttonWidth: '110%'
-
+        buttonWidth: '200%'
     });
     
       $('#sg_location').multiselect({
